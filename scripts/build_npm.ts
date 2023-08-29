8df0ef6d957118a6e889203de589489ad57ffd10
Cendyne <cendyne@cendyne.dev>
2023-08-29 18:51:37 -0500

    Bump document_ir version

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -12,9 +12,9 @@ await build({
   },
   test: false,
   mappings: {
-    "https://deno.land/x/document_ir@0.0.10/index.ts": {
+    "https://deno.land/x/document_ir@0.0.11/index.ts": {
       name: "document-ir",
-      version: "0.0.10",
+      version: "0.0.11",
     },
   },
   package: {
